220c8e73685e624c8e1753cfc2d25f471d13fb65
ppop472 <kakcsi02@gmail.com>
2024-03-27 12:57:59 +0100

    Delete Code directory

--- Code/selftest.py
+++
@@ -1,8 +0,0 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
-from kivy.clock import Clock
-from kivy.graphics import Color, Rectangle
-from kivy.properties import NumericProperty
-

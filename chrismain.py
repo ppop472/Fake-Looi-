20a8bf1f03f3097d80427434ead926afd0345da6
ppop472 <kakcsi02@gmail.com>
2024-04-09 09:30:05 +0200

    Add files via upload

--- chrismain.py
+++ chrismain.py
@@ -1,16 +1,17 @@
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Ellipse, Rectangle
+from kivy.graphics import Color, Ellipse, Rectangle, Line
 from kivy.graphics import Color, Rectangle, PushMatrix, PopMatrix, Rotate
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.animation import Animation
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.core.audio import SoundLoader
-from kivy.animation import Animation
 import time
 from kivy.vector import Vector
+import random
 import math
 from kivy.graphics import Color, RoundedRectangle
 
@@ -117,10 +118,15 @@ class RightEyeBrows(Widget):
             self.right_eyebrows = Rectangle(pos=(self.maxwidth * 0.6, self.maxheight * 0.72), 
                       size=(self.maxwidth * 0.2, self.maxheight * 0.015),
                       radius=border_radius)
-          
+
 class MyFloatLayout(FloatLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+        # AFK / VERDRIETIGE emotie/reactie
+        self.restart_idle_timer(instance=None)
+        self.boos_worden_bool = False
+        self.start_timer_reactie_sad(instance=None)
 
         # Variable of the screens
         Window.maximize()
@@ -140,6 +146,11 @@ class MyFloatLayout(FloatLayout):
         
         #Counter variabel voor de boos onclick ding
         self.counter_boos = 0
+
+        #Bink variableren :P
+        self.start_idle_timer(instance=None) # Idle animation start
+        rand_blonk = random.randint(7, 21) # Random seconds for blinking
+        Clock.schedule_interval(self.blink_animation, rand_blonk)  # Random Blinking
                 
         #Background
         with self.canvas.before:
@@ -177,6 +188,44 @@ class MyFloatLayout(FloatLayout):
         if right_eye_distance > max_distance_right:
             right_eye_distance = max_distance_right
 
+        
+        #new positie bullshit
+        left_eye_pupil_x = left_eye_distance * math.cos(left_eye_angle) + left_eye_center[0] - 10
+        left_eye_pupil_y = left_eye_distance * math.sin(left_eye_angle) + left_eye_center[1] - 10
+        right_eye_pupil_x = right_eye_distance * math.cos(right_eye_angle) + right_eye_center[0] - 10
+        right_eye_pupil_y = right_eye_distance * math.sin(right_eye_angle) + right_eye_center[1] - 10
+        
+        #lil position update
+        # self.left_eye.left_pupil.pos = (left_eye_pupil_x, left_eye_pupil_y)
+        # self.right_eye.right_pupil.pos = (right_eye_pupil_x, right_eye_pupil_y)
+
+         # Smooth animation using Kivy's Animation class
+        animation_left_eye = Animation(pos=(left_eye_pupil_x, left_eye_pupil_y), duration=0.2)
+        animation_right_eye = Animation(pos=(right_eye_pupil_x, right_eye_pupil_y), duration=0.2)
+
+        animation_left_eye.start(self.left_eye.left_pupil)
+        animation_right_eye.start(self.right_eye.right_pupil)
+
+    def update_pupils_drag(self, touch_x, touch_y):
+        left_eye_center = (self.maxwidth * 0.3, (self.maxheight * 0.5) - (self.maxwidth / 64))
+        right_eye_center = (self.maxwidth * 0.7,  (self.maxheight * 0.5) - (self.maxwidth / 64))
+
+        left_eye_distance = math.sqrt((touch_x - left_eye_center[0]) ** 2 + (touch_y - left_eye_center[1]) ** 2)
+        left_eye_angle = math.atan2(touch_y - left_eye_center[1], touch_x - left_eye_center[0])
+
+        right_eye_distance = math.sqrt((touch_x - right_eye_center[0]) ** 2 + (touch_y - right_eye_center[1]) ** 2)
+        right_eye_angle = math.atan2(touch_y - right_eye_center[1], touch_x - right_eye_center[0])
+
+        max_distance_left = self.maxwidth / 24
+        max_distance_right = self.maxwidth / 24
+
+        if left_eye_distance > max_distance_left:
+            left_eye_distance = max_distance_left
+
+        if right_eye_distance > max_distance_right:
+            right_eye_distance = max_distance_right
+
+        
         #new positie bullshit
         left_eye_pupil_x = left_eye_distance * math.cos(left_eye_angle) + left_eye_center[0] - 10
         left_eye_pupil_y = left_eye_distance * math.sin(left_eye_angle) + left_eye_center[1] - 10
@@ -186,31 +235,33 @@ class MyFloatLayout(FloatLayout):
         #lil position update
         self.left_eye.left_pupil.pos = (left_eye_pupil_x, left_eye_pupil_y)
         self.right_eye.right_pupil.pos = (right_eye_pupil_x, right_eye_pupil_y)
-    
+
     #On touch/drag ding
     def on_touch_down(self, touch):
         self.start_timer(instance=None)
         self.reset_timer()
         self.update_pupils(touch.x, touch.y)
+        self.restart_idle_timer(instance=None)
+        if not self.boos_worden_bool:
+            self.noreactie()
+            self.start_timer_reactie_sad(instance=None)
 
         #reacties
         self.boos_reactie(touch)
-        self.sad_var = False
-        self.niet_sad_worden()
-
-    def on_touch_move(self, touch):
-        self.start_timer(instance=None)
-        self.reset_timer()
-        self.update_pupils(touch.x, touch.y)
-
-        #reacties
-        self.sad_var = False
-        self.niet_sad_worden()
 
     def on_touch_up(self,touch):
-        #reacties
-        self.sad_var = True
-        self.niet_sad_worden()
+        if not self.boos_worden_bool:
+            self.noreactie()
+            self.start_timer_reactie_sad(instance=None)
+
+    def on_touch_move(self, touch):
+        self.restart_idle_timer(instance=None)
+        self.start_timer(instance=None)
+        self.reset_timer()
+        self.update_pupils_drag(touch.x, touch.y)
+        if not self.boos_worden_bool:
+            self.noreactie()
+            self.start_timer_reactie_sad(instance=None)
 
     #Timer voor de pupilen
     def start_timer(self, instance): 
@@ -241,15 +292,19 @@ class MyFloatLayout(FloatLayout):
         self.movement_in_progress = True
         Clock.schedule_interval(self.move_pupil_back_smoothly, 0.0001)
 
-    def move_pupil_back_smoothly(self): 
-        self.right_eye.right_pupil.pos = (self.desiredwidth * 6.90, self.desiredheight * 4.6)
-        self.left_eye.left_pupil.pos = (self.desiredwidth * 2.90,self.desiredheight * 4.6)
+    def move_pupil_back_smoothly(self):
+        right_eye_anim = Animation(pos=(self.desiredwidth * 6.90, self.desiredheight * 4.6),
+                                   duration=0.6, t='out_cubic')
+        right_eye_anim.start(self.right_eye.right_pupil)
+        left_eye_anim = Animation(pos=(self.desiredwidth * 2.90, self.desiredheight * 4.6),
+                                  duration=0.6, t='out_cubic')
+        left_eye_anim.start(self.left_eye.left_pupil)
         self.right_eye.canvas.ask_update()
+        self.start_idle_timer(instance=None)
 
     #Emotions/Reaction Yeaaaa
         
     def boos_reactie(self, touch):
-        self.sad_var = False
         touch_x, touch_y = touch.pos
         
         #Screen locatie van de boos right oog bepalen
@@ -283,12 +338,11 @@ class MyFloatLayout(FloatLayout):
         if self.counter_boos == 10:
             print("BOOS!", self.counter_boos)
             self.boos_worden()
+            self.counter_boos = 0
+            self.start_timer_reactie_sad(instance=None)
             
 
     def noreactie(self):
-        self.counter_boos = 0
-        self.sad_var = False
-
         #Ogen
         self.right_eye.right_pupil_color.rgba = [0, 0, 0, 1]
         self.left_eye.left_pupil_color.rgba = [0, 0, 0, 1]
@@ -300,10 +354,10 @@ class MyFloatLayout(FloatLayout):
         self.left_eyebrows.left_eyebrows_color.rgba = [1,1,1,1]
         self.left_eyebrows.rotate_left.angle = 0
 
-
         self.right_eyebrows.right_eyebrows_color.rgba = [1,1,1,1]
         self.right_eyebrows.rotate_right.angle = 0
         self.right_eyebrows.right_eyebrows.pos = (self.maxwidth * 0.6, self.maxheight * 0.72)
+        self.start_timer_reactie_sad(instance=None)
 
 # BOOS HAHAHAHAH
     def boos_worden(self):
@@ -317,6 +371,8 @@ class MyFloatLayout(FloatLayout):
         self.right_eyebrows.right_eyebrows_color.rgba = [0,0,0,1]
         self.right_eyebrows.rotate_right.angle = 10
         self.right_eyebrows.right_eyebrows.pos = (self.maxwidth * 0.6, self.maxheight * 0.751)
+
+        self.boos_worden_bool = True
 
         self.start_timer_reactie_boos(instance=None)
         self.reset_timer_reactie_boos()
@@ -339,14 +395,15 @@ class MyFloatLayout(FloatLayout):
         if elapsed_time_reactie_boos > self.time_limit_reactie_boos:
             self.counter_boos = 0
             Clock.unschedule(self.update_timer_reactie_boos)
+            self.boos_worden_bool = False
             self.noreactie()
             
-# SAD BRUHUUHUHU AFK REACTIE
-    def niet_sad_worden(self):
-        if self.sad_var:
-            print("true")
-        elif not self.sad_var:
-            print("false")
+# SAD BRUHUUHUHU AFK REACTIE     
+    def sad_worden_bepalen(self):
+        if not self.boos_worden_bool:
+            print("not boos sad")
+            self.sad_worden()
+        
             
     def sad_worden(self):
         self.left_eyebrows.left_eyebrows_color.rgba = [0,0,0,1]
@@ -358,7 +415,7 @@ class MyFloatLayout(FloatLayout):
         self.right_eyebrows.right_eyebrows.pos = (self.maxwidth * 0.6, self.maxheight * 0.685)
 
     def start_timer_reactie_sad(self, instance): 
-        self.time_limit_reactie_sad = 4
+        self.time_limit_reactie_sad = 30
         self.start_time_reactie_sad = time.time()     
         self.elapsed_time_reactie_sad = 0
         Clock.unschedule(self.update_timer_reactie_sad)
@@ -374,7 +431,76 @@ class MyFloatLayout(FloatLayout):
         if elapsed_time_reactie_sad > self.time_limit_reactie_sad:
             self.counter_sad = 0
             Clock.unschedule(self.update_timer_reactie_sad)
-            self.sad_worden()
+            self.sad_worden_bepalen()
+    
+    #Idle animatie DISDISDISDISDIS    
+    def start_idle_timer(self, instance):
+        self.time_limit = random.randint(3, 12)
+        self.idle_time = time.time()
+        self.elapsed_time = 0
+        self.idle_time = time.time()
+        Clock.unschedule(self.update_idle_timer)
+        Clock.schedule_interval(self.update_idle_timer, 0.1)
+
+    def update_idle_timer(self, dt):
+        current_time = time.time()
+        elapsed_time = current_time - self.idle_time
+        if elapsed_time > self.time_limit:
+            self.is_idle = True
+            Clock.unschedule(self.update_idle_timer)
+            Clock.schedule_interval(self.idle_animation, 3)
+
+    def reset_idle_timer(self):
+        self.idle_time = time.time()
+
+    def restart_idle_timer(self, instance):
+        self.is_idle = False
+        self.reset_idle_timer()
+        self.start_idle_timer(instance)
+        Clock.unschedule(self.idle_animation)
+
+    #Idle animation disdisdisdis
+    def idle_animation(self, dt):
+        # random direction vector
+        direction = Vector(random.uniform(-1, 1), random.uniform(-1, 1)).normalize()
+        # eye center calculation
+        left_eye_center = (self.maxwidth * 0.3, (self.maxheight * 0.5) - (self.maxwidth / 64))
+        right_eye_center = (self.maxwidth * 0.7, (self.maxheight * 0.5) - (self.maxwidth / 64))
+        # Give pupils position for goofy random
+        left_pupil_anim = Animation(pos=(left_eye_center[0] + direction[0] * self.maxwidth / 24,
+                                         left_eye_center[1] + direction[1] * self.maxwidth / 24), duration=0.6,
+                                    t='out_cubic')
+        right_pupil_anim = Animation(pos=(right_eye_center[0] + direction[0] * self.maxwidth / 24,
+                                          right_eye_center[1] + direction[1] * self.maxwidth / 24), duration=0.6,
+                                     t='out_cubic')
+        # quirky anim starts
+        left_pupil_anim.start(self.left_eye.left_pupil)
+        right_pupil_anim.start(self.right_eye.right_pupil)
+
+    #Knipperen KNIPKNIPKNIP HAHA
+    def blink_blonk(self, dt):
+        self.left_eye.left_pupil_color.a = 0
+        self.right_eye.right_pupil_color.a = 0
+        if self.boos_worden_bool:
+            self.left_eye.left_iris_color.rgba = (0.7, 0, 0, 1)
+            self.right_eye.right_iris_color.rgba = (0.7, 0, 0, 1)
+        elif not self.boos_worden_bool:
+            self.right_eye.right_iris_color.rgba = (0.59, 0.76, 0.85, 1)
+            self.left_eye.left_iris_color.rgba = (0.59, 0.76, 0.85, 1)
+
+    def un_blink_blonk(self, dt):
+        self.left_eye.left_pupil_color.a = 1
+        self.right_eye.right_pupil_color.a = 1
+        if self.boos_worden_bool:
+            self.left_eye.left_iris_color.rgba = (1, 0, 0, 0.8)
+            self.right_eye.right_iris_color.rgba = (1, 0, 0, 0.8)
+        elif not self.boos_worden_bool:
+            self.left_eye.left_iris_color.rgba = (0.69, 0.86, 0.95, 1)
+            self.right_eye.right_iris_color.rgba = (0.69, 0.86, 0.95, 1)
+
+    def blink_animation(self, dt):
+        Clock.schedule_once(self.blink_blonk)
+        Clock.schedule_once(self.un_blink_blonk, 0.2)
 
 class MyApp(App):
     def build(self):
